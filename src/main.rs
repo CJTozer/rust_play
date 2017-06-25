8823ec365019a1cdb25189e48810250f588edd82
Chris Tozer <chris.tozer@metaswitch.com>
2017-06-25 17:35:47 +0100

    Switch to testing out generics for watchdog

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,56 @@
-extern crate rand;
-extern crate ansi_term;
+/// Test of some pluggable architecutures in Rust
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 
-use std::io;
-use std::fmt;
-use rand::Rng;
-use ansi_term::Colour::Red;
+// Main thread spawns a set of WatchHandler objects
+// Each one of these has one watcher to look after
+// Watcher is started, given a channel to send regular heartbeats on
+// WatchHandler runs timers, checks there's a heartbeat
+// Main thread regularly checks each WatchHandler is OK
 
-#[derive(Debug)]
-struct Card {
-    suit: String,
-    value: String,
-    points: i8,
+enum HeartbeatStatus {
+    HeartbeatOK,
+    HeartbeatFailed,
 }
 
-impl Card {
-    fn new(i: &i8) -> Card {
-        let suit = match i / 13 {
-            // 0:♥, 1:♦, 2:♣, 3:♠
-            0 => Red.bold().paint("♥").to_string(),
-            1 => Red.bold().paint("♦").to_string(),
-            2 => "♣".to_string(),
-            3 => "♠".to_string(),
-            _ => "X".to_string(),
-        };
-        let (value, points) = match i % 13 {
-            // 0 => A
-            // 1-9 => 2-T
-            // 10, 11, 12 => J, Q, K
-            0 => ("A".to_string(), 1),
-            9 => ("T".to_string(), 10),
-            10 => ("J".to_string(), 10),
-            11 => ("Q".to_string(), 10),
-            12 => ("K".to_string(), 10),
-            x => ((x + 1).to_string(), x + 1),
-        };
-        Card {
-            suit: suit,
-            value: value,
-            points: points,
+trait Watcher {
+    // No new() for the trait object itself, just implementations.
+    fn new(Sender<HeartbeatStatus>) -> Self where Self:Sized;
+    fn start(&self);
+}
+
+struct DummyWatcher {
+    _heartbeat_tx: Sender<HeartbeatStatus>,
+}
+
+impl Watcher for DummyWatcher {
+    fn new(tx: Sender<HeartbeatStatus>) -> DummyWatcher {
+        DummyWatcher{
+            _heartbeat_tx: tx,
+        }
+    }
+
+    fn start(&self) {}
+}
+
+struct WatchHandler<'a> {
+    _ok: bool,
+    _heartbeat_rx: Receiver<HeartbeatStatus>,
+    _watcher: Box<Watcher + 'a>,
+}
+
+impl <'a> WatchHandler<'a> {
+    // Factory is probably better.
+    fn new() -> WatchHandler<'a> {
+        let (tx, rx): (Sender<HeartbeatStatus>, Receiver<HeartbeatStatus>) = mpsc::channel();
+        WatchHandler {
+            _ok: true,
+            // Just building a DummyWatcher for now.
+            _watcher: Box::new(DummyWatcher::new(tx)),
+            _heartbeat_rx: rx,
         }
     }
 }
 
-impl fmt::Display for Card {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}{}", self.value, self.suit)
-    }
-}
-
 fn main() {
-    // Shuffle the deck
-    // @@@ This is all a little messy - surely a standard way to create a 'random iterator'
-    let mut cards: Vec<i8> = (0..52).collect();
-    rand::thread_rng().shuffle(&mut cards);
-    // Convert into 52 Card objects
-    let card_objs: Vec<Card> = cards.iter().map(|x| Card::new(x)).collect();
-    let deck = card_objs.as_slice();
-    println!("{:?}", deck);
-    let mut draw_pile = deck.iter();
-
-    let mut hand = [draw_pile.next(), draw_pile.next()];
-    // @@@ Dealer cards...
-    // @@@ Show one dealer card?
-
-    loop {
-        // @@@ Pretty-print hand
-        println!("Your hand: {:?}", hand);
-
-        // @@@ Stick/Hit?
-        let mut action = String::new();
-        io::stdin().read_line(&mut action)
-            .expect("failed to read line");
-
-        // @@@ Match action => Stick/Hit
-    }
-
-    // @@@ Dealer plays.
-
-    // @@@ Calculate winner.
 }
